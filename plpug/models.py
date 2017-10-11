8af9fa74887062e20ce4fbb67c8b2b0bcc251226
Wojciech Zając <wojtek@reu.pl>
2017-10-11 19:50:00 +0200

    Events model

--- plpug/models.py
+++ plpug/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+class Event(models.Model):
+    """
+    Events
+    """
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_('Title'),
+    )
+    started_at = models.DateTimeField(
+        verbose_name=_('Started at'),
+    )
+    ended_at = models.DateTimeField(
+        verbose_name=_('Ended at'),
+    )
+    place = models.CharField(
+        verbose_name=_('Place'),
+    )
+    content = models.TextField(
+        verbose_name=_('Content'),
+    )
+    user = models.ForeignKey(
+        to=User,
+        related_name='event',
+        related_query_name='events',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at'),
+    )
+    updated_at = models.DateTimeField(
+        default=None,
+        null=True,
+        verbose_name=_('Updated at'),
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name=_('Is published'),
+    )
